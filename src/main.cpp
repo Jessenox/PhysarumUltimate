5004907aa1f56e2ffb57ddcc177af03799f281e2
joserojas4 <jose.rojas@cyttek.com>
2025-02-10 13:10:00 -0600

    Setting CMake project

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,13 @@ int main()
     auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
     window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
-
-        window.clear();
+        window.clear(sf::Color::Red);
         window.display();
     }
 }
